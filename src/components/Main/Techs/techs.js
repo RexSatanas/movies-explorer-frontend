646c8bd98d17f60e58c15c706609c5c00722c12e
Denis Myagotin <rexsatanas@yandex.ru>
2021-10-29 13:51:55 +0300

    main done

--- src/components/Main/Techs/techs.js
+++ src/components/Main/Techs/techs.js
@@ -5,7 +5,7 @@ function Techs() {
   return(
       <section className='techs'>
           <div className='aboutProject__container techs__container'>
-<h2 className='aboutProject__header-title techs__header-title'>Технологии</h2>
+          <h2 className='aboutProject__header-title techs__header-title'>Технологии</h2>
           <h3 className='techs__title'>7 технологий</h3>
           <p className='techs__subtitle'>На курсе веб-разработки мы освоили технологии,
               которые применили в дипломном проекте.</p>
